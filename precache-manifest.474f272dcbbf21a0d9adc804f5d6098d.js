f0f4b5ea67080a9f6a07246e7fde407983a16cce
ZhupanovOlexii <zhupanovolexii@gmail.com>
2020-04-23 16:33:31 +0300

    Updates

---
+++ precache-manifest.474f272dcbbf21a0d9adc804f5d6098d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d576e3f3ed9c9a27f03902638199f46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "227b1dd5e868986bab93",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "a3201fcccc3332d8eda5",
+    "url": "./static/css/main.687f6aae.chunk.css"
+  },
+  {
+    "revision": "227b1dd5e868986bab93",
+    "url": "./static/js/2.2b83b950.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2b83b950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3201fcccc3332d8eda5",
+    "url": "./static/js/main.c8f2f135.chunk.js"
+  },
+  {
+    "revision": "383414d4f5edafe01bf9",
+    "url": "./static/js/runtime-main.e9d4b854.js"
+  }
+]);
